3ad476f3e77b042270beae2ccc8ec1d11b63b29a
= <=>
2025-11-22 17:26:30 +0000

    wfc

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""This module defines a class Square with validation for the size attribute."""
+"""Module defining a Square class with size validation."""
+
 
 class Square:
     """Represents a square with a private size attribute."""
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
